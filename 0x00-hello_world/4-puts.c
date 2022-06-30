48cfe898d1e6d63e66a7cc590bf36382cf7de296
UniqueEl <umarfaruqadam03@gmail.com>
2022-06-30 12:16:43 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
-{
-	puts ("\"Programming is like building a multilingual puzzle");
+ {
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
