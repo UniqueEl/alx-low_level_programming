4e1776ac8aea443d7e03339a2870a6b39ea8aac7
UniqueEl <umarfaruqadam03@gmail.com>
2022-07-03 02:23:13 -0700

    hexdecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main-program entry point 
- * Return:0 if no error, non zero if error
+ *main-program entry point 
+ *Return:0 if no error, non zero if error
  */
 int main(void)
 {
