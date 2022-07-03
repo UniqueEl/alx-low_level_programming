199b55a912d132dc560b54d827acaa77f7cf6e89
UniqueEl <umarfaruqadam03@gmail.com>
2022-07-03 01:20:14 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main program entry point.
+ * return:0 if no error, non zero if error.
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+	return (0);
+}
