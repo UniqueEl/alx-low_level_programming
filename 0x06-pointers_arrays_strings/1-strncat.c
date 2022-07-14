e612cf1a21fdb28177c42b17616ca709d298dd4c
UniqueEl <umarfaruqadam03@gmail.com>
2022-07-14 07:51:41 -0700

    string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,19 +8,23 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
+	int i;
+	int j;
 
-	while (*(dest + count) != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		count++;
+		i++;
 	}
-	while (count2 < n)
+
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	return (dest)
+	dest[i] = '\0';
+
+	return (dest);
 }
