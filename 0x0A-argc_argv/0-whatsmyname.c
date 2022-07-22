6dc055ca517fc37b2a5e06fc51a16f86dfc17da8
UniqueEl <umarfaruqadam03@gmail.com>
2022-07-22 07:45:06 -0700

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,7 @@
-#include <main.h> 
+#include <stdio.h>
 /**
  * main - prints its name followed by a new line
  * @argc: number of command line arguments
-
-
-
-
-
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
